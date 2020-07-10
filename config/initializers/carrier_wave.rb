b5e0b494debd9b8cee3f36eef859fd144ab9d42d
ko-suke24 <kosuke0322@gmail.com>
2020-07-10 14:22:40 +0900

    本番環境での画像アップロード設定

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,11 @@
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory  = 'recipelog'
+  end
+end

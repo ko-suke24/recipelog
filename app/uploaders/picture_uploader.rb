6c12b8eac2a6e4b9df38ed518f55f2b63f7699b2
ko-suke24 <kosuke0322@gmail.com>
2020-07-11 09:18:51 +0900

    自動デプロイ２

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,11 +1,8 @@
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  storage :file
+  # storage :fog
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
